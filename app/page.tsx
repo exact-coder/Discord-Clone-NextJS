47a548bd6333caeadadeb8eafa08ae06e0b0113d
Programming Bees <hasanakash799@gmail.com>
2024-01-16 23:14:11 +0600

    chore: added shedcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-center text-cyan-50 font-extrabold text-4xl mt-80'>Wellcome to Discord Clone Mr.😎</h1>
+    <h1 className='text-center text-black-50 font-extrabold text-4xl mt-80'>Wellcome to Discord Clone Mr.😎</h1>
   )
 }
 
 // length 00.15min
+
