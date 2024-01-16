d628e84d4209f9c603fe01ab3b1ca0a45ec1d8bf
Programming Bees <hasanakash799@gmail.com>
2024-01-17 01:11:19 +0600

    chore: maintaing folder structure

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className='text-center text-indigo-500 font-extrabold text-4xl mt-80'>Wellcome to Discord Clone Mr.😎</h1>
+    </div>
+  )
+}
+
+// length 00.35min
+
