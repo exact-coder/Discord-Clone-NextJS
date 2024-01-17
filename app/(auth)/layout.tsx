885c4a0936eec8b0b51b7ecc909a5a4ea5264fba
Programming Bees <hasanakash799@gmail.com>
2024-01-17 22:48:33 +0600

    chore: added thememood by shadcn

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-export default function Layout() {
+const AuthLayout = ({ children}: {children: React.ReactNode}) => {
   return (
-    <div>Layout</div>
+    <div className="h-full flex items-center justify-center">{children}</div>
   )
 }
+
+export default AuthLayout;
 
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,12 +1,16 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div className="">
       <h1 className='text-center text-indigo-500 font-extrabold text-4xl mt-80'>Wellcome to Discord Clone Mr.😎</h1>
+      <UserButton afterSignOutUrl="/"/>
+      <ModeToggle/>
     </div>
   )
 }
 
-// length 00.35min
+// length 00.55min
 
