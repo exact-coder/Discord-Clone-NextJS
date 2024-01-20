f65593bf5d6ac3cbe4e51d7b3def3c9b29efd3c2
Programming Bees <hasanakash799@gmail.com>
2024-01-20 23:54:57 +0600

    chore: working for image file

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -7,6 +7,7 @@ import { Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,Dialog
 import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import FileUpload from '@/components/file-upload';
 
 
 const formSchema = z.object({
@@ -53,7 +54,13 @@ export default function InitialModal() {
                 <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
                     <div className="space-y-8 px-6">
                         <div className="flex items-center justify-center text-center">
-                            TODO: Image Upload
+                            <FormField control={form.control} name="imageUrl" render={({field}) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <FileUpload endpoint="serverImage" value={ field.value} onChange={field.onChange}/>
+                                    </FormControl>
+                                </FormItem>
+                            )} />
                         </div>
                         <FormField control={form.control} name="name" render={({field}) => (
                             <FormItem>
