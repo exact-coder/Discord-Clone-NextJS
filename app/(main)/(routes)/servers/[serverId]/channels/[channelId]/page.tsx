d82481452186b5ddfdd360ba8f90226c2feffd7e
Programming Bees <hasanakash799@gmail.com>
2024-02-02 20:47:02 +0600

    chore: added responsive side toggle button

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,10 +1,45 @@
-
-
+import ChatHeader from '@/components/chat/chat-header';
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const ChannelIdPage = () => {
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    }
+  });
+
+  if(!channel || !member){
+    redirect("/");
+  }
+
   return (
-    <div>ChannelIdPage</div>
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+        <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
+    </div>
   )
 }
 
