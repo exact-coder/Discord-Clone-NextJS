17c84cd96c222988283c48845512b0209531ad71
Programming Bees <hasanakash799@gmail.com>
2024-02-09 17:56:00 +0600

    chore: message scroll button

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -23,5 +23,5 @@ const SetupPage = async () => {
 
 export default SetupPage;
 
-// len 10.54min
+// len 11.00min
 
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,80 +1,116 @@
 "use client";
 
-import { Member, Message, Profile } from '@prisma/client';
-import React, { Fragment } from 'react';
-import ChatWelcome from './chat-welcome';
-import { useChatQuery } from '@/hooks/use-chat-query';
-import { Loader2, ServerCrash } from 'lucide-react';
-import { ChatItem } from './chat-item';
+import { Member, Message, Profile } from "@prisma/client";
+import React, { Fragment, useRef, ElementRef } from "react";
+import ChatWelcome from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
+import { ChatItem } from "./chat-item";
 import { format } from "date-fns";
-import { useChatSocket } from '@/hooks/use-chat-socket';
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
-    profile: Profile
-  }
-}
+    profile: Profile;
+  };
+};
 
 interface ChatMessagesProps {
-    name: string;
-    member: Member;
-    chatId: string;
-    apiUrl: string;
-    socketUrl: string;
-    socketQuery: Record<string, string>;
-    paramKey: "channelId" | "conversationId";
-    paramValue: string;
-    type: "channel" | "conversation";
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
 }
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
-const ChatMessages = ({name,member,chatId,apiUrl,socketUrl,socketQuery,paramKey,paramValue,type}: ChatMessagesProps) => {
+const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
   const addKey = `chat:${chatId}:messages`;
   const updateKey = `chat:${chatId}:messages:update`;
 
-  const {data,fetchNextPage,hasNextPage,isFetchingNextPage,status,} = useChatQuery({queryKey,apiUrl,paramKey,paramValue});
+  const chatRef = useRef<ElementRef<"div">>(null);
+  const bottomRef = useRef<ElementRef<"div">>(null);
 
-  useChatSocket({queryKey,addKey,updateKey});
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({ queryKey, apiUrl, paramKey, paramValue });
 
-  if(status === "pending"){
+  useChatSocket({ queryKey, addKey, updateKey });
+
+  if (status === "pending") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
-        <Loader2 className='h-7 w-7 text-zinc-500 animate-spin my-4'/>
-        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading messages...</p>
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
       </div>
-    )
+    );
   }
 
-  
-  if(status === "error"){
+  if (status === "error") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
-        <ServerCrash className='h-7 w-7 text-zinc-500 my-4'/>
-        <p className="text-xs text-zinc-500 dark:text-zinc-400">Something went wrong!</p>
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
       </div>
-    )
+    );
   }
-
 
   return (
-    <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
-        <div className="flex-1"/>
-        <ChatWelcome name={name} type={type} />
-        <div className="flex flex-col-reverse mt-auto">
-          {data?.pages?.map((group,i) => (
-            <Fragment key={i}>
-              {group.items.map((message: MessageWithMemberWithProfile) => (
-                <ChatItem key={message.id} id={message.id} currentMember={member} member={message.member} content={message.content} fileUrl={message.fileUrl} deleted={message.deleted} timestamp={format(new Date(message.createdAt), DATE_FORMAT)} isUpdated={message.updatedAt !== message.createdAt} socketUrl={socketUrl} socketQuery={socketQuery}  />
-              ))}
-            </Fragment>
-          ))}
+    <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
+      {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && <ChatWelcome name={name} type={type} />}
+      {hasNextPage && (
+        <div className="flex justify-center">
+          {isFetchingNextPage ? (
+          <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4"/>
+        ): (
+          <button onClick={() => fetchNextPage()} className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition"> Load previous message</button>
+        )}
         </div>
+      )}
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+      <div ref={bottomRef} />
     </div>
-  )
-}
+  );
+};
 
 export default ChatMessages;
-
-
